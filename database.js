cc7ba2fa6adc16865c7632ef209c6cfc627b6414
KINGD4LAL <hamzahdalal12@gmail.com>
2023-03-15 17:51:04 +0000

    DB_starter

---
+++ database.js
@@ -0,0 +1,108 @@
+const sqlite3 = require('sqlite3').verbose()
+const crypto = require("crypto")
+
+const DBSOURCE = "db.sqlite"
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      console.error(err.message)
+      throw err
+    }else{
+        console.log('Connected to the SQLite database.') 
+
+        db.run(`CREATE TABLE users (
+                    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    first_name text,
+                    last_name text,
+                    email text UNIQUE,
+                    password text,
+                    salt text,
+                    session_token text,
+                    CONSTRAINT email_unique UNIQUE (email)
+                )`,
+            (err) => {
+
+                if(err){
+                    console.log("Users table already created")
+                }else{
+                    console.log("Users table created")
+                }
+
+
+                const ADMIN_PASSWORD = "Admin123!"
+
+                const getHash = function(password, salt){
+                    return crypto.pbkdf2Sync(password, salt, 100000, 256, 'sha256').toString('hex');
+                };
+
+                const INSERT = 'INSERT INTO users (first_name, last_name, email, password, salt) VALUES (?,?,?,?,?)'
+                const salt = crypto.randomBytes(64);
+                const hash = getHash(ADMIN_PASSWORD, salt);
+
+                db.run(INSERT, ["admin", "admin", "admin@admin.com", hash, salt.toString('hex')], (err) => {
+                    if(err){
+                        console.log("Admin account already exists")
+                    } 
+                })
+            }
+        )
+
+        db.run(`CREATE TABLE feedRecipes (
+                    recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title text,
+                    ingredients text,
+                    directions text,
+                    date_published INTEGER,
+                    date_edited INTEGER,
+                    created_by INTEGER,
+                    FOREIGN KEY(created_by) REFERENCES users(user_id)
+                )`,
+            (err) => {
+                if(err){
+                    console.log("Feed Recipes table already created")
+                }else{
+                    console.log("Feed Recipes table created")
+                }
+            }
+        )
+        db.run(`CREATE TABLE ratings (
+            rating_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            rating INTEGER,
+            date_published INTEGER,
+            recipe_id INTEGER,
+            posted_by INTEGER,
+            FOREIGN KEY(recipe_id) REFERENCES feedRecipes(recipe_id),
+            FOREIGN KEY(posted_by) REFERENCES users(user_id)
+        )`,
+    (err) => {
+        if(err){
+            console.log("Ratings table already created")
+        }else{
+            console.log("Ratings table created")
+        }
+    }
+)
+        db.run(`CREATE TABLE savedRecipes (
+            recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title text,
+            ingredients text,
+            date_published INTEGER,
+            saved_by INTEGER,
+            FOREIGN KEY(saved_by) REFERENCES users(user_id)
+        )`,
+    (err) => {
+        if(err){
+            console.log("Saved Recipes table already created")
+        }else{
+            console.log("Saved Recipes table created")
+        }
+    }
+)
+
+  
+
+    }
+});
+
+
+module.exports = db
