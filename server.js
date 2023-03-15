6b495f80cc797fbbaa91a1d8aa6687729358595a
Ali Ahmed <sibkhan131@gmail.com>
2023-03-15 22:58:40 +0000

    incorporating database.js with server.js
    This change will allow the database.js script to be run when server.js is run upon running npm run dev in the terminal

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const morgan = require('morgan')
 const bodyParser = require("body-parser");
 const cors = require("cors")
+const db = require('./database.js');
 
 
 const app = express()
