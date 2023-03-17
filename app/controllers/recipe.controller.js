7bcd9bf45202535ecf2ab03b96d0b9eb5e8d7df9
aliahmed786 <sibkhan131@gmail.com>
2023-03-17 03:06:33 +0000

    + Functionality for additional recipe endpoints

--- app/controllers/recipe.controller.js
+++ app/controllers/recipe.controller.js
@@ -33,8 +33,81 @@ const postRecipe = (req,res) => {
         })
     })
 }
+const updateRecipe = (req, res) => {
+    let recipe_id = parseInt(req.params.recipe_id);
+    recipe.getSingleRecipe(recipe_id, (err, recipeData) => { // changed parameter name to recipeData
+        if (err === 404) return res.sendStatus(404)
+        if (err) return res.sendStatus(500)
 
+        const schema = Joi.object({
+            "title": Joi.string(),
+            "ingredients": Joi.string(),
+            "directions": Joi.string()
+        })
+        const {
+            error
+        } = schema.validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        if (req.body.hasOwnProperty("title")) {
+            recipeData.title = req.body.title // changed variable name to recipeData
+        }
+        if (req.body.hasOwnProperty("ingredients")) {
+            recipeData.ingredients = req.body.ingredients // changed variable name to recipeData
+        }
+        if (req.body.hasOwnProperty("directions")) {
+            recipeData.directions = req.body.directions // changed variable name to recipeData
+        }
+        recipe.updateRecipe(recipeData, (err) => { // changed variable name to recipeData
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500)
+            }
+            return res.sendStatus(200)
+
+        })
+    })
+}
+
+const deleteRecipe = (req, res) => {
+    let recipe_id = parseInt(req.params.recipe_id);
+    recipe.getSingleRecipe(recipe_id, (err, recipeData) => {
+        if (err === 404) return res.sendStatus(404)
+        if (err) return res.sendStatus(500)
+        recipe.deleteRecipe(recipe_id, (err) => {
+            if (err) return res.sendStatus(500);
+            return res.sendStatus(200);
+        })
+    })
+}
+
+const getOne = (req, res) => {
+    let recipe_id = parseInt(req.params.recipe_id);
+    recipe.getSingleRecipe(recipe_id, (err, recipe) => {
+        if (err === 404) return res.sendStatus(404)
+        if (err) return res.sendStatus(500)
+        return res.status(200).send(recipe)
+    })
+}
+
+const getUserRecipes =(req,res) => {
+    let token = req.get('X-Authorization')
+    users.getIdFromToken(token,(err,user_id) => {
+        if(err || user_id === null) {
+            return res.sendStatus(401)
+        }
+    recipe.getUserRecipes(user_id,(err, num_rows, results) => {
+        if(err) return res.sendStatus(500)
+
+        return res.status(200).send(results)
+    })
+    })
+}
 module.exports = {
     getAll: getAll,
-    postRecipe:postRecipe
+    getOne:getOne,
+    postRecipe:postRecipe,
+    updateRecipe:updateRecipe,
+    deleteRecipe:deleteRecipe,
+    getUserRecipes:getUserRecipes
 }
--- app/models/recipe.model.js
+++ app/models/recipe.model.js
@@ -32,8 +32,70 @@ const postNewRecipe = (recipes,id, done) => {
         return done(null,this.lastID)
     })
 }
+const getSingleRecipe = (id, done) => {
+    const sql = 'SELECT * FROM feedRecipes WHERE recipe_id=?'
+    db.get(sql, [id], (err, row) => {
+        if (err) return done(err)
+        if (!row) return done(404)
+        recipe = {
+            recipe_id: row.recipe_id,
+            title: row.title,
+            ingredients: row.ingredients,
+            directions: row.directions,
+            date_published: new Date(row.date_published).toLocaleDateString(),
+            date_edited: new Date(row.date_edited).toLocaleDateString(),
+            created_by: row.created_by
+        }
+        return done(null, recipe)
+
+    })
+}
+
+const updateRecipe = (recipe, done) => {
+    const sql = 'UPDATE feedRecipes SET title=?,ingredients=?,directions=? WHERE recipe_id=?'
+    let values = [recipe.title, recipe.ingredients, recipe.directions, recipe.recipe_id]
+    db.run(
+        sql,
+        values,
+        (err) => {
+            return done(err)
+        }
+    )
+}
+
+const deleteRecipe = (recipe_id, done) => {
+    const sql = 'DELETE FROM feedRecipes WHERE recipe_id=?';
+    db.run(sql, [recipe_id], (err) => {
+        return done(err)
+
+    })
+}
+const getUserRecipes = (id,done) => {
+    const results = []
+    const sql="SELECT * FROM feedRecipes WHERE created_by=?"
+    db.each(sql,[id],(err,row) => {
+        if(err) return done(err)
+
+        results.push({
+            recipe_id: row.recipe_id,
+            title: row.title,
+            ingredients: row.ingredients,
+            directions: row.directions,
+            date_published: new Date(row.date_published).toLocaleDateString(),
+            date_edited: new Date(row.date_edited).toLocaleDateString(),
+            created_by: row.created_by
+        })
+    },
+    (err,num_rows) => {
+        return done(err,num_rows,results)
+    })
+}
 
 module.exports = {
     getAllRecipes:getAllRecipes,
-    postNewRecipe:postNewRecipe
+    postNewRecipe:postNewRecipe,
+    getSingleRecipe:getSingleRecipe,
+    deleteRecipe:deleteRecipe,
+    updateRecipe:updateRecipe,
+    getUserRecipes:getUserRecipes
 }
